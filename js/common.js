00afc4239155659bcabc8cb7db149237867474dc
Donchenko Eugine <donchenkoeugine@gmail.com>
2017-07-15 21:00:50 +0300

    Added saving to cookie

--- js/common.js
+++ js/common.js
@@ -1,38 +1,76 @@
-let button = document.querySelector("button");
+(function() {
+    let createBtn = document.getElementById("create");
+    let saveBtn = document.getElementById("save");
+    let cookie = document.cookie;
+    let coloredFig={length:0,
+                    elements:[]};
+    const maxWidthFig=200;
+    const maxHeightFig=200;
+    const minWidthFig=10;
+    const minHeightFig=10;
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-button.addEventListener("click", ()=>{
-    let div = document.createElement("div");
-    div.style.width     = getRandomInt(10,200) + "px";
-    div.style.height    = getRandomInt(10,200) + "px";
-    div.style.backgroundColor     = `rgb(${getRandomInt(0,255)},${getRandomInt(0,255)},${getRandomInt(0,255)})`;
-    div.style.left      = getRandomInt(0, 1200) + "px";
-    div.style.top       = getRandomInt(0, 500) + "px";
-    document.body.appendChild(div);
-})
-
-let activeElem, offsetX, offsetY;
-document.addEventListener("mousedown", (event)=>{
-    if (event.target!==button) {
-        activeElem=event.target;
-        offsetX = event.offsetX;
-        offsetY = event.offsetY;
-        console.log(event.target);
+    if (cookie) {
+        cookie=cookie.split(";");
+        cookie=cookie.map(item=>{return item.trim()});
+        for (let i=0; i<cookie.length;i++) {
+            if (cookie[i].indexOf("coloredFig=")==0) {
+                coloredFig=JSON.parse(cookie[i].substring(11));
+                break;
+            }
+        }
     }
-})
-document.addEventListener("mouseup", (event)=>{
-    if (event.target!==button) {
-        activeElem=null;
+    
+    if (coloredFig.length>0) {
+        // console.log(coloredFig);
+        for (let i=0; i<coloredFig.length; i++) {
+            createFigure(coloredFig.elements[i]);
+        }
     }
-})
-document.addEventListener("mousemove", (event)=>{
-    if (activeElem) {
-        activeElem.style.left=(event.clientX-offsetX)+"px";
-        activeElem.style.top=(event.clientY-offsetY)+"px";
-    };
-})
+
+    function createFigure(obj){
+        let div = document.createElement("div");
+        div.style.width     = obj.width + "px";
+        div.style.height    = obj.height + "px";
+        div.style.backgroundColor     = obj.color;
+        div.style.left      = obj.left + "px";
+        div.style.top       = obj.top + "px";
+        document.body.appendChild(div);       
+    }
+
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+
+    createBtn.addEventListener("click", ()=>{
+        let div = document.createElement("div");
+        coloredFig.length++;
+        coloredFig.elements.push({color: `rgb(${getRandomInt(0,255)},${getRandomInt(0,255)},${getRandomInt(0,255)})`, width: getRandomInt(minWidthFig,maxWidthFig), height: getRandomInt(minHeightFig,maxHeightFig), left: getRandomInt(0, screen.availWidth-maxWidthFig), top: getRandomInt(0, screen.availHeight-maxHeightFig-50)});
+        createFigure(coloredFig.elements[coloredFig.length-1]);
+        document.body.appendChild(div);
+    })
+
+    saveBtn.addEventListener("click", ()=>{
+        document.cookie="coloredFig="+JSON.stringify(coloredFig);
+    })
+    let activeElem, offsetX, offsetY;
+    document.addEventListener("mousedown", (event)=>{
+        if (event.target.tagName!=="BUTTON") {
+            activeElem=event.target;
+            offsetX = event.offsetX;
+            offsetY = event.offsetY;
+        }
+    })
+    document.addEventListener("mouseup", (event)=>{
+        if (event.target.tagName!=="BUTTON") {
+            activeElem=null;
+        }
+    })
+    document.addEventListener("mousemove", (event)=>{
+        if (activeElem) {
+            activeElem.style.left=(event.clientX-offsetX)+"px";
+            activeElem.style.top=(event.clientY-offsetY)+"px";
+        };
+    })
+})()
